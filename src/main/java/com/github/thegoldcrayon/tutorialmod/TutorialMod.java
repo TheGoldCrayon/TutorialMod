29c67dfb571d9a4053bca941110b54caf219e0d3
TheGoldCrayon <thegoldcrayon101@gmail.com>
2019-11-17 14:02:38 -0600

    Initial Commit

---
+++ src/main/java/com/github/thegoldcrayon/tutorialmod/TutorialMod.java
@@ -0,0 +1,11 @@
+package com.github.thegoldcrayon.tutorialmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TutorialMod.MODID)
+public class TutorialMod
+{
+
+    public static final String MODID = "tutorialmod";
+
+}
