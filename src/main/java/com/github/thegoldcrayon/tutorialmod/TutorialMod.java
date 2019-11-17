ad4e01ab23f8bc1ed9a20c138c86ff72446773a5
TheGoldCrayon <thegoldcrayon101@gmail.com>
2019-11-17 14:16:26 -0600

    Update

--- src/main/java/com/github/thegoldcrayon/tutorialmod/TutorialMod.java
+++ src/main/java/com/github/thegoldcrayon/tutorialmod/TutorialMod.java
@@ -1,11 +1,22 @@
 package com.github.thegoldcrayon.tutorialmod;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(TutorialMod.MODID)
 public class TutorialMod
 {
 
     public static final String MODID = "tutorialmod";
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public TutorialMod()
+    {
+
+        LOGGER.debug("Hello from the Tutorial Mod!");
+
+    }
 
 }
