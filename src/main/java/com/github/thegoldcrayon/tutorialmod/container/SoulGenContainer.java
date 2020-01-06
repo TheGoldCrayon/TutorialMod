ab8d1e938fea5ae7f899b31d0d6c131662891768
Phil <thegoldcrayon101@gmail.com>
2020-01-05 18:48:53 -0600

    stuffs

--- src/main/java/com/github/thegoldcrayon/tutorialmod/container/SoulGenContainer.java
+++ src/main/java/com/github/thegoldcrayon/tutorialmod/container/SoulGenContainer.java
@@ -1,4 +1,39 @@
 package com.github.thegoldcrayon.tutorialmod.container;
 
-public class SoulGenContainer {
+import com.github.thegoldcrayon.tutorialmod.init.ModBlocks;
+import com.github.thegoldcrayon.tutorialmod.init.ModContainerTypes;
+import com.github.thegoldcrayon.tutorialmod.tileentity.SoulGenTileEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.util.IWorldPosCallable;
+
+import javax.annotation.Nonnull;
+
+public class SoulGenContainer extends Container
+{
+
+    public final SoulGenTileEntity tileEntity;
+    private final IWorldPosCallable canInteractWithCallable;
+
+    public SoulGenContainer(final int windowID, final SoulGenTileEntity tileEntity, final PlayerInventory playerInventory, final PlayerEntity player)
+    {
+
+        super(ModContainerTypes.SOUL_GEN, windowID);
+
+        canInteractWithCallable = IWorldPosCallable.of(tileEntity.getWorld(), tileEntity.getPos());
+        this.tileEntity = tileEntity;
+
+        //addSlots(tileEntity, playerInventory);
+
+    }
+
+    @Override
+    public boolean canInteractWith(@Nonnull final PlayerEntity player)
+    {
+
+        return isWithinUsableDistance(canInteractWithCallable, player, ModBlocks.SOUL_GEN);
+
+    }
+
 }
--- src/main/java/com/github/thegoldcrayon/tutorialmod/init/ModContainerTypes.java
+++ src/main/java/com/github/thegoldcrayon/tutorialmod/init/ModContainerTypes.java
@@ -1,6 +1,7 @@
 package com.github.thegoldcrayon.tutorialmod.init;
 
 import com.github.thegoldcrayon.tutorialmod.TutorialMod;
+import com.github.thegoldcrayon.tutorialmod.container.SoulGenContainer;
 import com.github.thegoldcrayon.tutorialmod.container.TutorialGeneratorContainer;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraftforge.registries.ObjectHolder;
@@ -10,5 +11,6 @@ public class ModContainerTypes
 {
 
     public static final ContainerType<TutorialGeneratorContainer> TUTORIAL_GENERATOR = null;
+    public static final ContainerType<SoulGenContainer> SOUL_GEN = null;
 
 }
